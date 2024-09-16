bcfc799d01a0cf02728b745823fed53c71b26602
Ignas Baranauskas <ibaranau@redhat.com>
2024-09-16 12:00:13 +0100

    test

---
+++ ui-tests/tests/0_basic_ray.test.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { test } from '@jupyterlab/galata';
+
+import { expect } from '@playwright/test';
+
+import * as path from 'path';
+
+test.setTimeout(460000);
+
+test.describe('Widget Visual Regression', () => {
+  test.beforeEach(async ({ page, tmpPath }) => {
+    await page.contents.uploadDirectory(
+      path.resolve(__dirname, './notebooks'),
+      tmpPath
+    );
+    await page.filebrowser.openDirectory(tmpPath);
+  });
+
+  test('Run notebook widgets.ipynb and capture cell outputs', async ({
+    page,
+    tmpPath,
+  }) => {
+    const notebook = '0_basic_ray.ipynb';
+    await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+    await page.notebook.activate(notebook);
+
+    const captures = new Array<Buffer>();
+    const cellCount = await page.notebook.getCellCount();
+
+    await page.notebook.runCellByCell({
+      onAfterCellRun: async (cellIndex: number) => {
+        const cell = await page.notebook.getCellOutput(cellIndex);
+        if (cell && (await cell.isVisible())) {
+          captures.push(await cell.screenshot());
+        }
+      },
+    });
+
+    await page.notebook.save();
+
+    for (let i = 0; i < cellCount; i++) {
+      const image = `widgets-cell-${i}.png`;
+      expect.soft(captures[i]).toMatchSnapshot(image);
+    }
+  });
+});
